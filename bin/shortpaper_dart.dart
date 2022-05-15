bd6246c5471cce5aee8037cf142ae8547ce53b68
ArtuKILL <39144443+ArtuKILL@users.noreply.github.com>
2022-05-14 23:05:38 -0400

    build: Primer commit

---
+++ bin/shortpaper_dart.dart
@@ -0,0 +1,5 @@
+import 'package:shortpaper_dart/shortpaper_dart.dart' as shortpaper_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${shortpaper_dart.calculate()}!');
+}
---
+++ test/shortpaper_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:shortpaper_dart/shortpaper_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
